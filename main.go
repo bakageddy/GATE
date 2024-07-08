326f6c331cf4344aea9bc4dde67f6430e6df2aa3
Dinesh Kumar <gdineshda@gmail.com>
2024-07-08 20:33:29 +0530

    File Server: Did nothing

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+
+	"github.com/bakageddy/gate/templates"
 )
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if err := templates.Home().Render(context.Background(), w); err != nil {
+		log.Println("Failed to render template")
+	}
+	w.WriteHeader(http.StatusInternalServerError)
+}
 
 func main() {
 	handler := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static", handler))
+	http.HandleFunc("/", home)
 	http.ListenAndServe(":8080", nil)
 }
